bec0024d2cfd1fe80563fb2af2203136c6ff2055
Christian Paling <christian.paling@googlemail.com>
2020-12-30 21:45:52 +0100

    Set up emacs for bibtex

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,7 @@
 ;; Use MELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 (package-initialize)
 
 ;; Emacs uses this, do not touch !
@@ -10,9 +11,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "a06658a45f043cd95549d6845454ad1c1d6e24a99271676ae56157619952394a" "939ea070fb0141cd035608b2baabc4bd50d8ecc86af8528df9d41f4d83664c6a" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" default))
+   (quote
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "a06658a45f043cd95549d6845454ad1c1d6e24a99271676ae56157619952394a" "939ea070fb0141cd035608b2baabc4bd50d8ecc86af8528df9d41f4d83664c6a" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" default)))
  '(package-selected-packages
-   '(go-mode zones visual-fill-column ivy-rich yaml-mode neotree flycheck-pos-tip rjsx-mode js2-mode add-node-modules-path inf-ruby exec-path-from-shell company lsp-mode web-mode gruvbox-theme general evil-collection magit doom-modeline markdown-mode cider parinfer clojure-mode flycheck-clj-kondo flycheck rainbow-delimiters projectile org-bullets smex which-key key-chord counsel evil)))
+   (quote
+    (org-plus-contrib go-mode zones visual-fill-column ivy-rich yaml-mode neotree flycheck-pos-tip rjsx-mode js2-mode add-node-modules-path inf-ruby exec-path-from-shell company lsp-mode web-mode gruvbox-theme general evil-collection magit doom-modeline markdown-mode cider parinfer clojure-mode flycheck-clj-kondo flycheck rainbow-delimiters projectile org-bullets smex which-key key-chord counsel evil))))
 
 ;; Emacs uses this, do not touch !
 (custom-set-faces
